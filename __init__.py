bf0d38095747a5fc38645afe73b3c08d70d27dff
ShinyMonkey-droid <adriannalumen@gmail.com>
2026-02-28 08:10:17 -0500

    Initial Commit

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 # Importing the app to initialize
 from emotion_detection import emotion_detector
 
-result = emotion_detector()
+text_to_analyze = "I hate working long hours."
+result = emotion_detector(text_to_analyze)
 print(result)
